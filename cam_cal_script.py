475a9f39ff5419b0fcfe1b59b891a57aa10a831b
Vegnics <pe_linareso@hotmail.com>
2019-01-30 18:44:00 -0500

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -4,6 +4,6 @@ from CeleryPy import log
 from DB import DB
 import Capture 
 import Image
-[x,y,z]=DB().getcoordinates(test_coordinates=True)
+[x,y,z]=DB().getcoordinates()
 log('funcaaaaa', message_type='error', title='FUNCO')
 
