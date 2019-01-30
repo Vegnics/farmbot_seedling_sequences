628877972b17e94f8d5d8ae30e8e8bc4a1c525c9
Vegnics <pe_linareso@hotmail.com>
2019-01-30 18:56:54 -0500

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -4,6 +4,6 @@ from CeleryPy import log
 from DB import DB
 import Capture 
 import Image
-x=DB().getcoordinates().coordinates
-log('funcaaaaa', message_type='error', title='FUNCO')
+x='daleeeeee'
+log(x, message_type='error', title='FUNCO')
 
