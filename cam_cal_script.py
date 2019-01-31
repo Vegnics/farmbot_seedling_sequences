538a9551f8af0b78da757e7c6474c44be958fb85
Vegnics <pe_linareso@hotmail.com>
2019-01-30 19:07:11 -0500

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -4,6 +4,6 @@ from CeleryPy import log
 from DB import DB
 import Capture 
 import Image
-x='daleeeeee'
-log(print x, message_type='error', title='FUNCO')
+x="daleeeeee"
+log(print(x,end=''), message_type='error', title='FUNCO')
 
