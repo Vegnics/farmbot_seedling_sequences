48df01166c2bb3702738398ee2cff79974c27e14
Vegnics <pe_linareso@hotmail.com>
2019-02-01 11:40:17 -0500

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -5,5 +5,7 @@ from DB import DB
 import Capture 
 import Image
 x=DB.getcoordinates()
-log('ya', message_type='error', title='FUNCO')
+#log('ya', message_type='error', title='FUNCO')
+log_message = '[{x}]'.format(x=x)
+send_message(log_message,'error')
 
