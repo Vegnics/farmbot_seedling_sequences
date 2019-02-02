201b12d9f3efa7a28b9d858bbc3c078bde0c109c
Vegnics <pe_linareso@hotmail.com>
2019-02-02 13:35:32 -0600

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -6,10 +6,13 @@ from DB import DB
 import Capture
 import Image
 import requests
+import subprocess
+import json
 x=DB()
 y=x.get_image(50)
+z=json.loads(y)
 #y=x._get_bot_state()
 #y=dict(y)['user_env']['camera']
 #log(print(os.environ['API_TOKEN']), message_type='error', title='FUNCO')
-y=str(y)
+z=str(z)
 send_message(message=y, message_type='success', channel='toast')
