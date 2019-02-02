846ca1605ca8b5012ec2cf8d09999a233acb5765
Vegnics <pe_linareso@hotmail.com>
2019-02-02 12:32:27 -0600

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -9,6 +9,6 @@ import requests
 x=DB()
 #y=x.api_get('peripherals/')
 y=x._get_bot_state()
-y=str(y)
+y=dict(y)['user_env']['camera']
 #log(print(os.environ['API_TOKEN']), message_type='error', title='FUNCO')
 send_message(message=y, message_type='success', channel='toast')
