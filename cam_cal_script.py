5ff13c610c7c3251fd67bc50e115d9568ae9b2d9
Vegnics <pe_linareso@hotmail.com>
2019-02-02 16:04:19 -0600

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -8,16 +8,18 @@ import requests
 
 x=DB()
 response = x.api_get('images/' + str(52))
+img_json=response.json()
 #filename_path = upload_path(y)
 #retcode = call(["raspistill", "-w", "640", "-h", "480", "-o", filename_path])
 #y=x._get_bot_state()
 #y=dict(y)['user_env']['camera']
 #log(print(os.environ['API_TOKEN']), message_type='error', title='FUNCO')
 #z=str(z)
-send_message(message=str(response.json()), message_type='success', channel='toast')
+send_message(message=str(img_json), message_type='success', channel='toast')
 #z=os.path.dirname(os.path.realpath(__file__)) + os.sep
 #send_message(message=z, message_type='success', channel='toast')
-y = requests.get(url, stream=True)
+image_url = img_json['attachment_url']
+y = requests.get(image_url, stream=True)
 send_message(message=str(y), message_type='success', channel='toast')
 #y=x.get_image(52)
 
