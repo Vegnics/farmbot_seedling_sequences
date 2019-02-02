3edc897a389fd120d26af5264eabb5ebef6bfa7c
Vegnics <pe_linareso@hotmail.com>
2019-02-02 14:13:35 -0600

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -4,14 +4,18 @@ from CeleryPy import log
 from CeleryPy import send_message
 from DB import DB
 import Capture
-import Image
+from Image import Image
+from Parameters import Parameters
 import requests
 import subprocess
 import json
-x=DB()
+database=DB()
+params=Parameters()
+imag=Image(params,database)
 #y=x.get_image(50)
 #y=x._get_bot_state()
 #y=dict(y)['user_env']['camera']
 #log(print(os.environ['API_TOKEN']), message_type='error', title='FUNCO')
 #z=str(z)
-send_message(message=str(help('json')), message_type='success', channel='toast')
+imag.show()
+send_message(message='bien'), message_type='success', channel='toast')
