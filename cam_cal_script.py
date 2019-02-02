4ce16a43e7b6c6623457aeba5b14cfa02f9e51f8
Vegnics <pe_linareso@hotmail.com>
2019-02-02 14:37:07 -0600

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -3,8 +3,20 @@ import sys
 from CeleryPy import log
 from CeleryPy import send_message
 from DB import DB
+from subprocess import call
+
+def upload_path(filename):
+    try:
+        images_dir = os.environ['IMAGES_DIR']
+    except KeyError:
+        images_dir = '/tmp/images'
+    path = images_dir + os.sep + filename
+return path
+
 x=DB()
 y=x.get_image(50)
+filename_path = upload_path(y)
+retcode = call(["raspistill", "-w", "640", "-h", "480", "-o", filename_path])
 #y=x._get_bot_state()
 #y=dict(y)['user_env']['camera']
 #log(print(os.environ['API_TOKEN']), message_type='error', title='FUNCO')
