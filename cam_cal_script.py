d672bfbd87852bd8087d636730f92244dcba1180
Vegnics <pe_linareso@hotmail.com>
2019-02-02 20:01:23 -0600

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -4,24 +4,8 @@ from CeleryPy import log
 from CeleryPy import send_message
 from DB import DB
 import json
+from Parameters import Parameters
 import requests
 
 x=DB()
-#response = x.api_get('images/' + str(52))
-#img_json=response.json()
-#filename_path = upload_path(y)
-#retcode = call(["raspistill", "-w", "640", "-h", "480", "-o", filename_path])
-#y=x._get_bot_state()
-#y=dict(y)['user_env']['camera']
-#log(print(os.environ['API_TOKEN']), message_type='error', title='FUNCO')
-#z=str(z)
-#send_message(message=str(img_json), message_type='success', channel='toast')
-#z=os.path.dirname(os.path.realpath(__file__)) + os.sep
-#send_message(message=z, message_type='success', channel='toast')
-#attachment=img_json['attachment_url']
-#image_url = 'http://192.168.1.106:8000'+attachment[0:-11]
-#image_name=img_json['meta']['name']
-#send_message(message=str(image_url), message_type='success', channel='toast')
-#y = requests.get(image_url, stream=True)
-#y=x._download_image_from_url(image_name,image_url)
 y=x.get_image(52)
