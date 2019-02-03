e3672af1aba6b0412c54ae42d2efa8fb827ea6f2
Vegnics <pe_linareso@hotmail.com>
2019-02-02 19:38:28 -0600

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -23,5 +23,5 @@ image_url = 'http://192.168.1.106:8000'+attachment[0:-11]
 image_name=img_json['meta']['name']
 send_message(message=str(image_url), message_type='success', channel='toast')
 #y = requests.get(image_url, stream=True)
-y=x._download_image_from_url(image_name,image_url)
-
+#y=x._download_image_from_url(image_name,image_url)
+y=x.get_image(52)
