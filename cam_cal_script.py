4d63c0925eb6283763d7e9d55c3b61bb0b53d03c
Vegnics <pe_linareso@hotmail.com>
2019-02-02 20:07:58 -0600

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -3,9 +3,13 @@ import sys
 from CeleryPy import log
 from CeleryPy import send_message
 from DB import DB
+from Image import Image
 import json
 from Parameters import Parameters
 import requests
 
 x=DB()
 y=x.get_image(52)
+parms=Parameters()
+z=Image(parms,x)
+z.show()
