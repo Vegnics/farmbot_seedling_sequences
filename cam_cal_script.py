c6a484f4d5664005b1ed60c8fdecee6335cc8007
Vegnics <pe_linareso@hotmail.com>
2019-02-02 22:03:15 -0600

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -12,16 +12,15 @@ from subprocess import call
 
 x=DB()
 y=x.get_image(52)
-parms=Parameters()
-z=Image(parms,x)
-z.load(y)
+#parms=Parameters()
+#z=Image(parms,x)
+#z.load(y)
 #print(cv2.__version__)
 #img = cv2.imread(y,0)
-print(str(img))
+#print(str(img))
 print(sys.version)
 #cv2.imshow('image',img)
 #z.show()
-retcode = call(
-["raspistill", "-w", "640", "-h", "480", "-o", y])
+retcode = call(["raspistill", "-w", "640", "-h", "480", "-o", y])
 #help(cv2.imshow)
 
