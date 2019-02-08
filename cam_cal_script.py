04cdfd547b8a2127686f0dd599e6ac693bea1bbc
Vegnics <pe_linareso@hotmail.com>
2019-02-08 18:56:57 -0500

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -10,17 +10,15 @@ import requests
 import cv2
 from subprocess import call
 
-x=DB()
-y=x.get_image(52)
+#x=DB()
+#y=x.get_image(52)
 #parms=Parameters()
 #z=Image(parms,x)
 #z.load(y)
 #print(cv2.__version__)
-#img = cv2.imread(y,0)
+img = cv2.imread('/tmp/images/1549669210.jpg',1)
+send_message(message='Hello World!', message_type='success', channel='toast')
 #print(str(img))
-print(sys.version)
-#cv2.imshow('image',img)
-#z.show()
-retcode = call(["raspistill", "-w", "640", "-h", "480", "-o", y])
-#help(cv2.imshow)
+
+
 
