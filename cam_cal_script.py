a808f6bb1c31db3d791c25dabf64dd1ef8489e52
Vegnics <pe_linareso@hotmail.com>
2019-02-08 19:14:56 -0500

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -18,16 +18,8 @@ import numpy as np
 #z.load(y)
 #print(cv2.__version__)
 img1 = cv2.imread('/tmp/images/1549669210.jpg',1)
-img2=cv2.resize(img1,(640,480),interpolation = cv2.INTER_AREA)
-def create_mask(image,lowergreen,uppergreen):##función para crear máscara a partir de valores máximos y minimos de HSV
-  imghsv=cv2.cvtColor(image,cv2.COLOR_BGR2HSV_FULL)
-  mask=cv2.inRange(imghsv,lowergreen,uppergreen)
-  return mask
-def integrate(hist,bounds):#función para integrar un histograma
-    aux=0
-    for i in range(bounds[0],bounds[1]+1,1):
-        aux=aux+hist[i]
-    return aux
+img2 = cv2.resize(img1,(640,480),interpolation = 3)
+
 
 send_message(message='Hello World!', message_type='success', channel='toast')
 
