12f515e43e564b69366a52d0373a9c5101bf41c3
Vegnics <pe_linareso@hotmail.com>
2019-02-08 19:19:39 -0500

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -17,9 +17,11 @@ import numpy as np
 #z=Image(parms,x)
 #z.load(y)
 #print(cv2.__version__)
-img1 = cv2.imread('/tmp/images/1549669210.jpg',1)
-#img2 = cv2.resize(img1,(640,480),interpolation = 3)
-
+img2 = cv2.imread('/tmp/images/1549669210.jpg',1)
+def create_mask(image,lowergreen,uppergreen):##función para crear máscara a partir de valores máximos y minimos de HSV
+  imghsv=cv2.cvtColor(image,cv2.COLOR_BGR2HSV_FULL)
+  mask=cv2.inRange(imghsv,lowergreen,uppergreen)
+  return mask
 
 send_message(message='Hello World!', message_type='success', channel='toast')
 
