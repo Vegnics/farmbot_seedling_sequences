3d9962e0156fcd95b61c1e93c9b91c5208a95855
Vegnics <pe_linareso@hotmail.com>
2019-02-09 08:08:01 -0600

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -18,7 +18,7 @@ import numpy as np
 #z.load(y)
 #print(cv2.__version__)
 img1 = cv2.imread('/tmp/images/1549133011.jpg',1)
-img2=cv2.resize(img1,(640,480),interpolation = cv2.INTER_AREA)
+help(cv2.resize())
 
 send_message(message=str(cv2.__version__), message_type='success', channel='toast')
 
