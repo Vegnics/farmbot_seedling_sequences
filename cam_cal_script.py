93259a88b88b6e5a929179d8edb200507140e01f
Vegnics <pe_linareso@hotmail.com>
2019-02-08 19:08:43 -0500

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -28,33 +28,9 @@ def integrate(hist,bounds):#función para integrar un histograma
     for i in range(bounds[0],bounds[1]+1,1):
         aux=aux+hist[i]
     return aux
-def colorize(image):##función para cambiar el brillo y el contraste de imagen
- n_image = np.zeros(image.shape, image.dtype)
- image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
- histo = cv2.calcHist([image_gray], [0], None, [256], [0, 256])
- histo_itg=integrate(histo,[249,255])
- alpha=2.21046+(2.519272e-06)*histo_itg
- beta=-155+-0.001259636217060513*histo_itg
- for y in range(image.shape[0]):
-    for x in range(image.shape[1]):
-        for c in range(image.shape[2]):
-            n_image[y, x, c] = np.clip(np.multiply(alpha,image[y, x, c])+beta, 0, 255)
- return n_image
 
-new_image=colorize(img2)##obtenemos imagen con brillo y contraste modificados
-########SETEAMOS VALORES MÍNIMOS Y MÁXIMOS DE HSV##################
-HL=51
-SL=90
-VL=140
-HH=112
-SH=255
-VH=255
-###################################################################
-mask=create_mask(new_image,np.array([HL,SL,VL]),np.array([HH,SH,VH]))###Creamos la máscara
-image3=cv2.bitwise_and(new_image,new_image,mask=mask)##aplicamos la máscara
-image3=cv2.medianBlur(image3,7)
 send_message(message='Hello World!', message_type='success', channel='toast')
-#print(str(img))
+
 
 
 
