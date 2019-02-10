4b31801ae5a608f915bb00e8ea6ad5191d1b7eb9
Vegnics <pe_linareso@hotmail.com>
2019-02-09 23:23:18 -0600

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -33,10 +33,10 @@ def colorize(image):##función para cambiar el brillo y el contraste de imagen
 new_image=colorize(img2)##obtenemos imagen con brillo y contraste modificados
 cv2.imwrite('/tmp/images/1549138022.jpg',new_image)
 ########SETEAMOS VALORES MÍNIMOS Y MÁXIMOS DE HSV##################
-HL=56
-SL=5
+HL=100
+SL=80
 VL=5
-HH=130
+HH=150
 SH=255
 VH=255
 ###################################################################
