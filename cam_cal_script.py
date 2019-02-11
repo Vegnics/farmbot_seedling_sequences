597bdf1a78e16b9ee62883e3d6d5ae6d539df943
Vegnics <pe_linareso@hotmail.com>
2019-02-11 18:52:56 -0500

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -34,7 +34,7 @@ new_image=colorize(img2)##obtenemos imagen con brillo y contraste modificados
 cv2.imwrite('/tmp/images/1549138022.jpg',new_image)
 ########SETEAMOS VALORES MÍNIMOS Y MÁXIMOS DE HSV##################
 HL=52
-SL=150
+SL=130
 VL=85
 HH=100
 SH=255
@@ -49,7 +49,7 @@ PD = PlantDetection(
             blur=5, morph=2, iterations=5, from_env_var=True, coordinates=True,
             array=[{"size": 7, "kernel": 'ellipse', "type": 'dilate',  "iters": 4},
                    {"size": 3, "kernel": 'ellipse', "type": 'erode', "iters": 2}],
-            HSV_min=[52,150,85],HSV_max=[100,255,210]
+            HSV_min=[52,130,85],HSV_max=[100,255,210]
             )
 PD.detect_plants() # detect coordinates and sizes of weeds and plants
 
