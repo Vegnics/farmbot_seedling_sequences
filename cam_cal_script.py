6a0dc57d9051ba4560d3fb317da1479cc8b129d2
Vegnics <pe_linareso@hotmail.com>
2019-02-11 18:22:02 -0500

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -25,8 +25,8 @@ def create_mask(image,lowergreen,uppergreen):##función para crear máscara a pa
   return mask
 def colorize(image):##función para cambiar el brillo y el contraste de imagen
  n_image = np.zeros(image.shape, image.dtype)
- alpha=2.32
- beta=-450
+ alpha=2.4
+ beta=-460
  n_image = np.clip(np.multiply(alpha,image)+beta, 0, 255)
  return n_image.astype(np.uint8) 
 
