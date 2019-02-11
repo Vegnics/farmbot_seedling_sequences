63a67176950dee4970fae53c2e99ec770c3bb00c
Vegnics <pe_linareso@hotmail.com>
2019-02-11 18:17:15 -0500

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -34,7 +34,7 @@ new_image=colorize(img2)##obtenemos imagen con brillo y contraste modificados
 cv2.imwrite('/tmp/images/1549138022.jpg',new_image)
 ########SETEAMOS VALORES MÍNIMOS Y MÁXIMOS DE HSV##################
 HL=52
-SL=100
+SL=80
 VL=85
 HH=100
 SH=255
@@ -42,14 +42,14 @@ VH=210
 ###################################################################
 mask=create_mask(new_image,np.array([HL,SL,VL]),np.array([HH,SH,VH]))###Creamos la máscara
 image3=cv2.bitwise_and(new_image,new_image,mask=mask)##aplicamos la máscara
-#image3=cv2.medianBlur(image3,7)
+image3=cv2.medianBlur(image3,7)
 cv2.imwrite('/tmp/images/1549138027.jpg',image3)
 PD = PlantDetection(
             image='/tmp/images/1549138027.jpg',
             blur=5, morph=2, iterations=5, from_env_var=True, coordinates=True,
             array=[{"size": 5, "kernel": 'ellipse', "type": 'dilate',  "iters": 2},
                    {"size": 3, "kernel": 'ellipse', "type": 'erode', "iters": 2}],
-            HSV_min=[52,100,85],HSV_max=[100,255,210]
+            HSV_min=[60,80,85],HSV_max=[100,255,210]
             )
 PD.detect_plants() # detect coordinates and sizes of weeds and plants
 
