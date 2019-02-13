77bbb72a45d67d2b04949077f63019562ac0b440
Vegnics <pe_linareso@hotmail.com>
2019-02-13 09:54:05 -0500

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -48,7 +48,7 @@ cv2.imwrite('/tmp/images/1549138027.jpg',image3)
 PD = PlantDetection(
             image='/tmp/images/1549138027.jpg',
             blur=5, morph=2, iterations=5, from_env_var=True, coordinates=True,
-            array=[{"size": 7, "kernel": 'ellipse', "type": 'dilate',  "iters": 2},
+            array=[{"size": 7, "kernel": 'ellipse', "type": 'dilate',  "iters": 1},
                    {"size": 3, "kernel": 'ellipse', "type": 'erode', "iters": 2}],
             HSV_min=[49,95,50],HSV_max=[115,255,255]
             )
@@ -62,7 +62,7 @@ for coordinate_location in PD.plant_db.coordinate_locations:
 send_message(message='TUDO BEM', message_type='success', channel='toast')
 CeleryPy.move_absolute((500,500,-400),(0,0,0),100)
 for coordinate_location in PD.plant_db.coordinate_locations:
-   if coordinate_location[2] > 24:
+   if coordinate_location[2] > 16:
         CeleryPy.move_absolute((coordinate_location[0],coordinate_location[1],-430),(0,0,0),80)
 #CeleryPy.move_absolute((500,500,0),(0,0,0),100)
 #CeleryPy.move_absolute((0,0,0),(0,0,0),100)
