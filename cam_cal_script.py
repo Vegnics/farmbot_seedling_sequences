908a126c5c40fc7d249eab17d4c7b8d054238d74
Vegnics <pe_linareso@hotmail.com>
2019-02-15 11:49:58 -0500

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -72,7 +72,7 @@ CeleryPy.write_pin(number=4, value=0, mode=0)
 for coordinate_location in PD.plant_db.coordinate_locations:
    if coordinate_location[2] > 14:
         x=coordinate_location[0]
-        y=coordinate_location[1]+4
+        y=coordinate_location[1]
         CeleryPy.move_absolute((x,y,-235),(0,0,0),100)
         CeleryPy.move_absolute((x,y,-273),(0,0,0),100)
         CeleryPy.wait(500)
