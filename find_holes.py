a58ba702b4944ffc4544c5ee50d9c9ea4bf338c9
Vegnics <pe_linareso@hotmail.com>
2019-02-25 12:07:10 -0500

    Update find_holes.py

--- find_holes.py
+++ find_holes.py
@@ -42,5 +42,42 @@ def circles(template):
 circles(img2)##obtenemos circulos
 new_image=img2
 cv2.imwrite('/tmp/images/1549138022.jpg',new_image)
+########SETEAMOS VALORES MÍNIMOS Y MÁXIMOS DE HSV##################
+HL=52
+SL=95
+VL=50
+HH=110
+SH=255
+VH=255
+###################################################################
+mask=create_mask(new_image,np.array([HL,SL,VL]),np.array([HH,SH,VH]))###Creamos la máscara
+image3=cv2.bitwise_and(new_image,new_image,mask=mask)##aplicamos la máscara
+image3=cv2.medianBlur(image3,5)
+cv2.imwrite('/tmp/images/1549138027.jpg',image3)
 
+PD = PlantDetection(
+            image='/tmp/images/1549138027.jpg',
+            blur=5, morph=2, iterations=5, from_env_var=True, coordinates=True,
+            array=[{"size": 3, "kernel": 'ellipse', "type": 'dilate',  "iters": 3},
+                   {"size": 5, "kernel": 'ellipse', "type": 'erode', "iters": 3}],
+            HSV_min=[49,95,50],HSV_max=[110,255,255]
+            )
+PD.detect_plants() # detect coordinates and sizes of weeds and plants
+if len(PD.plant_db.coordinate_locations) >= 1:
+  for coordinate_location in PD.plant_db.coordinate_locations:
+        log("Plant detected at X = {:5.0f} mm, Y = {:5.0f} mm with R = {:.1f} mm".format(
+                    coordinate_location[0],
+                    coordinate_location[1],
+                    coordinate_location[2]))
+  send_message(message='TUDO BEM', message_type='success', channel='toast')
+  for coordinate_location in PD.plant_db.coordinate_locations:
+       x=coordinate_location[0]
+       y=coordinate_location[1]
+       CeleryPy.move_absolute((x,y,-235),(0,0,0),100)
+       CeleryPy.wait(500)
+if len(PD.plant_db.coordinate_locations) == 0:
+  send_message(message='NO HOLES', message_type='error', channel='toast')
+CeleryPy.move_absolute((0,0,0),(0,0,0),250)
+  #CeleryPy.move_absolute((500,500,0),(0,0,0),100)
+#CeleryPy.move_absolute((0,0,0),(0,0,0),100)
 
