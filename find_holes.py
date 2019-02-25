564928388a777e350296101fabb2beea47d8c789
Vegnics <pe_linareso@hotmail.com>
2019-02-25 12:10:53 -0500

    Update find_holes.py

--- find_holes.py
+++ find_holes.py
@@ -58,8 +58,7 @@ cv2.imwrite('/tmp/images/1549138027.jpg',image3)
 PD = PlantDetection(
             image='/tmp/images/1549138027.jpg',
             blur=5, morph=2, iterations=5, from_env_var=True, coordinates=True,
-            array=[{"size": 3, "kernel": 'ellipse', "type": 'dilate',  "iters": 3},
-                   {"size": 5, "kernel": 'ellipse', "type": 'erode', "iters": 3}],
+            array=[{"size": 5, "kernel": 'ellipse', "type": 'erode', "iters": 4}],
             HSV_min=[49,95,50],HSV_max=[110,255,255]
             )
 PD.detect_plants() # detect coordinates and sizes of weeds and plants
