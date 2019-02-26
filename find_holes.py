54d4cc54f879a47b9f905ade65f5d2f53c65f809
Vegnics <pe_linareso@hotmail.com>
2019-02-26 10:44:48 -0500

    Update find_holes.py

--- find_holes.py
+++ find_holes.py
@@ -62,7 +62,7 @@ cv2.imwrite('/tmp/images/1549138027.jpg',image3)
 
 PD = PlantDetection(
             image='/tmp/images/1549138027.jpg',
-            blur=1, morph=1, iterations=1, from_env_var=True, coordinates=True,
+            blur=2, morph=2, iterations=3, from_env_var=True, coordinates=True,
             array=[{"size": 5, "kernel": 'ellipse', "type": 'erode', "iters": 4}],
             HSV_min=[49,95,50],HSV_max=[110,255,255]
             )
