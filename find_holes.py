5d086005ecbb430de7ab7a0f451e5cb75356ecc8
Vegnics <pe_linareso@hotmail.com>
2019-02-26 10:24:42 -0500

    Update find_holes.py

--- find_holes.py
+++ find_holes.py
@@ -81,7 +81,7 @@ if len(PD.plant_db.coordinate_locations) >= 1:
     #   CeleryPy.wait(500)
 if len(PD.plant_db.coordinate_locations) == 0:
   send_message(message='NO HOLES', message_type='error', channel='toast')
-CeleryPy.move_absolute((0,0,0),(0,0,0),250)
+#CeleryPy.move_absolute((0,0,0),(0,0,0),250)
   #CeleryPy.move_absolute((500,500,0),(0,0,0),100)
 #CeleryPy.move_absolute((0,0,0),(0,0,0),100)
 
