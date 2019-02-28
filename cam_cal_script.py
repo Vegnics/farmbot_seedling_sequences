b37936d283bbe4e68f1fbc10b46674d81f239161
Vegnics <pe_linareso@hotmail.com>
2019-02-28 13:45:06 -0500

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -64,6 +64,7 @@ PD.detect_plants() # detect coordinates and sizes of weeds and plants
 if len(PD.plant_db.coordinate_locations) >= 1:
   dir_path='/root/farmware'
   matrix=np.load(dir_path+'/'+'array.npy')
+  log(str(matrix))
   for coordinate_location in PD.plant_db.coordinate_locations:
         log("Plant detected at X = {:5.0f} mm, Y = {:5.0f} mm with R = {:.1f} mm".format(
                     coordinate_location[0],
