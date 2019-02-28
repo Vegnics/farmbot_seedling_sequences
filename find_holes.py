b023e2d222e62d7682dbf0523a592f25c286bb6f
Vegnics <pe_linareso@hotmail.com>
2019-02-28 18:25:26 -0500

    Update find_holes.py

--- find_holes.py
+++ find_holes.py
@@ -122,12 +122,12 @@ if len(PD.plant_db.coordinate_locations) >= 1:
   matrix=fill_array(matrix,holes) 
   send_message(message='TUDO BEM', message_type='success', channel='toast')
   np.save('/root/farmware/array',matrix)
-  CeleryPy.move_absolute((550,450,-435),(0,0,0),200)
+  CeleryPy.move_absolute((550,450,-270),(0,0,0),200)
   for i in range(rows):
     for j in range(cols):
       x=matrix[i,j,0]-5
       y=matrix[i,j,1]+5
-      CeleryPy.move_absolute((int(x),int(y),-435),(0,0,0),60)
+      CeleryPy.move_absolute((int(x),int(y),-270),(0,0,0),60)
 if len(PD.plant_db.coordinate_locations) == 0:
   send_message(message='NO HOLES', message_type='error', channel='toast')
 #CeleryPy.move_absolute((0,0,0),(0,0,0),250)
