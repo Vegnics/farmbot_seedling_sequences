031a2ac2cf856b3e58d35d5d6e7cd4c91c4f8904
Vegnics <pe_linareso@hotmail.com>
2019-03-07 13:21:46 -0500

    Update testopencl.py

--- testopencl.py
+++ testopencl.py
@@ -1,5 +1,5 @@
 import CeleryPy
 import cv2
 
-CeleryPy.log(str(cv2.ocl.setUseOpenCL(True)))
-CeleryPy.log(str((cv2.ocl.haveOpenCL())))
+CeleryPy.log(str(cv2.ocl.haveOpenCL()))
+CeleryPy.log(str(cv2.getNumberOfCPUs()))
