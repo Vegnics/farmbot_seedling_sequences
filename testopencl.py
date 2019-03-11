cea5b1417de81e60a7e4f007acd0a908b2e571ea
Vegnics <pe_linareso@hotmail.com>
2019-03-11 13:30:32 -0500

    Update testopencl.py

--- testopencl.py
+++ testopencl.py
@@ -5,10 +5,10 @@ cmd='ls {}'.format(dir_path)
 url='https://bootstrap.pypa.io/get-pip.py'
 try:
     from urllib import request
-    request.urlretrieve(url, '{}/get-pip.py'.format(dir_path))
-    f = os.popen(cmd)
-    out = f.read()
-    CeleryPy.send_message(message=str(out), message_type='success', channel='toast')
+    #request.urlretrieve(url, '{}/get-pip.py'.format(dir_path))
+    #f = os.popen(cmd)
+    #out = f.read()
+    CeleryPy.send_message(message='GOOD', message_type='success', channel='toast')
 except ImportError:
     CeleryPy.send_message(message='Module error', message_type='error', channel='toast')
 
