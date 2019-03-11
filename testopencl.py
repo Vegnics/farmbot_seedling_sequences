5f03528329aac110ef91adf2985092b420e34004
Vegnics <pe_linareso@hotmail.com>
2019-03-11 11:56:34 -0500

    Update testopencl.py

--- testopencl.py
+++ testopencl.py
@@ -3,9 +3,8 @@ import cv2
 import os
 
 CeleryPy.log(str(cv2.ocl.haveOpenCL()))
-CeleryPy.log(str(cv2.getNumberOfCPUs()))
-cmd='pip install cython'
-f=os.popen(cmd)
-out=f.read()
-CeleryPy.log(str(out))
+dir_path = os.path.dirname(os.path.realpath(__file__))
+getpip=dir_path+'/get-pip.py'
+cmd='python3 {}'.format(getpip)
+os.system(cmd)
 
