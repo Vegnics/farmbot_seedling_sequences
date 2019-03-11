81fa4c66451e6fcc92b1b1c250d74f1acb62d95a
Vegnics <pe_linareso@hotmail.com>
2019-03-11 12:40:09 -0500

    Update testopencl.py

--- testopencl.py
+++ testopencl.py
@@ -2,9 +2,10 @@ import CeleryPy
 import cv2
 import os
 
-CeleryPy.log(str(cv2.ocl.haveOpenCL()))
 dir_path = os.path.dirname(os.path.realpath(__file__))
-getpip=dir_path+'/get-pip.py'
-cmd='python3 {}'.format(getpip)
-os.system(cmd)
+cmd='ls {}'.format(dir_path)
+f=os.popen(cmd)
+out=f.read()
+CeleryPy.send_message(message=str(out), message_type='warn', channel='toast'):
+
 
