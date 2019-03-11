be871fdf119d40bd3257837d82bd811c756cbe4a
Vegnics <pe_linareso@hotmail.com>
2019-03-11 13:45:03 -0500

    Update testopencl.py

--- testopencl.py
+++ testopencl.py
@@ -2,13 +2,16 @@ import CeleryPy
 import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 cmd='ls {}'.format(dir_path)
-url='https://bootstrap.pypa.io/get-pip.py'
+url='https://raw.githubusercontent.com/Vegnics/files_probe/master/get-pip.py'
 try:
     from urllib.request import urlretrieve
-    urlretrieve(url, '{}/get-pip.py'.format(dir_path))
-    #f = os.popen(cmd)
-    #out = f.read()
-    CeleryPy.send_message(message='GOOD', message_type='success', channel='toast')
+    try:
+        urlretrieve(url, '{}/get-pip.py'.format(dir_path))
+        f = os.popen(cmd)
+        out = f.read()
+        CeleryPy.send_message(message=str(out), message_type='success', channel='toast')
+    except:
+        CeleryPy.send_message(message='File not found', message_type='error', channel='toast')
 except ImportError:
     CeleryPy.send_message(message='Module error', message_type='error', channel='toast')
 
