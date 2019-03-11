aa396df4d040dfbe307537d3f42757a3694738b4
Vegnics <pe_linareso@hotmail.com>
2019-03-11 14:20:47 -0500

    Update testopencl.py

--- testopencl.py
+++ testopencl.py
@@ -1,19 +1,11 @@
 import CeleryPy
 import os
-dir_path = os.path.dirname(os.path.realpath(__file__))
-cmd='python3'+'{}'.format(dir_path)+'/get-pip.py'
-url='http://192.168.0.6:8000/Desktop/get-pip.py'
-try:
-    from urllib.request import urlretrieve
-    try:
-        urlretrieve(url, '{}/get-pip.py'.format(dir_path))
-        f = os.popen(cmd)
-        out = f.read()
-        CeleryPy.send_message(message=str(out), message_type='success', channel='toast')
-    except:
-        CeleryPy.send_message(message='File not found', message_type='error', channel='toast')
-except ImportError:
-    CeleryPy.send_message(message='Module error', message_type='error', channel='toast')
+cmd='pip install sklearn'
+f = os.popen(cmd)
+out = f.read()
+CeleryPy.send_message(message=str(out), message_type='success', channel='toast')
+
+
 
 
 
