b85c682833a4f2e36b9b2a46911b4fe88ed4faab
Vegnics <pe_linareso@hotmail.com>
2019-03-11 10:14:21 -0500

    Update testopencl.py

--- testopencl.py
+++ testopencl.py
@@ -1,8 +1,12 @@
 import CeleryPy
 import cv2
 import os
+import subprocess
 
 CeleryPy.log(str(cv2.ocl.haveOpenCL()))
 CeleryPy.log(str(cv2.getNumberOfCPUs()))
-cmd="sudo apt-get install python3-pip"
-os.system(cmd)
+out = subprocess.Popen(['sudo','apt-get','install','python3-pip'], 
+           stdout=subprocess.PIPE, 
+           stderr=subprocess.STDOUT)
+CeleryPy.log(str(stdout))
+CeleryPy.log(str(stderr))
