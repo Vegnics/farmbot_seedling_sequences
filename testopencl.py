00a35961922d82e56894e44fdfef491bb9a16c46
Vegnics <pe_linareso@hotmail.com>
2019-03-11 13:35:40 -0500

    Update testopencl.py

--- testopencl.py
+++ testopencl.py
@@ -4,8 +4,8 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 cmd='ls {}'.format(dir_path)
 url='https://bootstrap.pypa.io/get-pip.py'
 try:
-    from urllib import request
-    request.urlretrieve(url, '{}/get-pip.py'.format(dir_path))
+    from urllib.request import urlretrieve
+    urlretrieve(url, '{}/get-pip.py'.format(dir_path))
     #f = os.popen(cmd)
     #out = f.read()
     CeleryPy.send_message(message='GOOD', message_type='success', channel='toast')
