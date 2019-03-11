852d25b4cbfcd58a416b28ab49bb477baadbfe90
Vegnics <pe_linareso@hotmail.com>
2019-03-11 13:27:38 -0500

    Update testopencl.py

--- testopencl.py
+++ testopencl.py
@@ -1,12 +1,18 @@
 import CeleryPy
-from urllib import request
 import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 cmd='ls {}'.format(dir_path)
 url='https://bootstrap.pypa.io/get-pip.py'
-request.urlretrieve(url, '{}/get-pip.py'.format(dir_path))
-f=os.popen(cmd)
-out=f.read()
-CeleryPy.send_message(message=str(out), message_type='success', channel='toast')
+try:
+    from urllib import request
+    request.urlretrieve(url, '{}/get-pip.py'.format(dir_path))
+    f = os.popen(cmd)
+    out = f.read()
+    CeleryPy.send_message(message=str(out), message_type='success', channel='toast')
+except ImportError:
+    CeleryPy.send_message(message='Module error', message_type='error', channel='toast')
+
+
+
 
 
