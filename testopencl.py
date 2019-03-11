a97b1982e7d650f38c11207f65be69c4d6d07548
Vegnics <pe_linareso@hotmail.com>
2019-03-11 13:07:05 -0500

    Update testopencl.py

--- testopencl.py
+++ testopencl.py
@@ -1,9 +1,10 @@
 import CeleryPy
-import cv2
+from urllib import request
 import os
-
 dir_path = os.path.dirname(os.path.realpath(__file__))
 cmd='ls {}'.format(dir_path)
+url='https://bootstrap.pypa.io/get-pip.py'
+request.urlretrieve(url, '{}/get-pip.py'.format(dir_path))
 f=os.popen(cmd)
 out=f.read()
 CeleryPy.send_message(message=str(out), message_type='success', channel='toast')
