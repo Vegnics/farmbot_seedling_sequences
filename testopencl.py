51cbcb4337e7e283ebc0e2c25dde060ef84a5799
Vegnics <pe_linareso@hotmail.com>
2019-03-11 12:46:10 -0500

    Update testopencl.py

--- testopencl.py
+++ testopencl.py
@@ -6,6 +6,6 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 cmd='ls {}'.format(dir_path)
 f=os.popen(cmd)
 out=f.read()
-CeleryPy.send_message(message=str(out), message_type='warn', channel='toast'):
+CeleryPy.send_message(message=str(out), message_type='success', channel='toast')
 
 
