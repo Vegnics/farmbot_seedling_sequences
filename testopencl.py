d7eab7316c914f477341b5e3f0799feea3a750b1
Vegnics <pe_linareso@hotmail.com>
2019-03-11 10:29:29 -0500

    Update testopencl.py

--- testopencl.py
+++ testopencl.py
@@ -5,7 +5,9 @@ import os
 CeleryPy.log(str(cv2.ocl.haveOpenCL()))
 CeleryPy.log(str(cv2.getNumberOfCPUs()))
 cmd='sudo apt-get install python3-pip'
-f=os.popen(cmd)
+cmd2='farmbot2019'
+os.system(cmd)
+f=os.popen(cmd2)
 out=f.read()
 CeleryPy.log(str(out))
 
