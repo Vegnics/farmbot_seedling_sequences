129efcfcc47f16582a1043175ec53cfabe5ef8c7
Vegnics <pe_linareso@hotmail.com>
2019-03-11 10:17:05 -0500

    Update testopencl.py

--- testopencl.py
+++ testopencl.py
@@ -8,5 +8,6 @@ CeleryPy.log(str(cv2.getNumberOfCPUs()))
 out = subprocess.Popen(['sudo','apt-get','install','python3-pip'], 
            stdout=subprocess.PIPE, 
            stderr=subprocess.STDOUT)
+stdout,stderr = out.communicate()
 CeleryPy.log(str(stdout))
 CeleryPy.log(str(stderr))
