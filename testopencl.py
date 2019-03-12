b564008d11608f8cbcaec9c77d61c76e35c5ac0b
Vegnics <pe_linareso@hotmail.com>
2019-03-12 12:47:46 -0500

    Update testopencl.py

--- testopencl.py
+++ testopencl.py
@@ -2,6 +2,7 @@ import CeleryPy
 import os
 import subprocess
 proc = subprocess.Popen(['locate','LOGGING'],shell=True)
+outs, errs = proc.communicate()
 CeleryPy.send_message(message='TUDO BEM', message_type='success', channel='toast')
 
 
