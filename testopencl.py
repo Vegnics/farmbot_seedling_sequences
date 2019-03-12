78ebb63659aa4b5fc4cceefd2d7466f50aee8da3
Vegnics <pe_linareso@hotmail.com>
2019-03-12 12:45:01 -0500

    Update testopencl.py

--- testopencl.py
+++ testopencl.py
@@ -1,13 +1,8 @@
 import CeleryPy
 import os
 import subprocess
-proc = subprocess.Popen(['locate','LOGGING'])
-try:
-    outs, errs = proc.communicate(timeout=15)
-except TimeoutExpired:
-    proc.kill()
-    outs, errs = proc.communicate()
-CeleryPy.send_message(message=str(outs), message_type='success', channel='toast')
+proc = subprocess.Popen(['locate','LOGGING'],shell=True)
+CeleryPy.send_message(message='TUDO BEM', message_type='success', channel='toast')
 
 
 
