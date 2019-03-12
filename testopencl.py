6c764d45fccd6aefaeb44bc5de86289c28993f04
Vegnics <pe_linareso@hotmail.com>
2019-03-12 14:20:42 -0500

    Update testopencl.py

--- testopencl.py
+++ testopencl.py
@@ -6,7 +6,8 @@ def find_all(name, path):
         if name in files or name in dirs:
             result.append(os.path.join(root, name))
     return result
-result=find_all('pip','/')
+f=os.popen('ls /root/.cache/pip')
+result=f.read()
 CeleryPy.send_message(message='YEAH'+'\n'+str(result), message_type='success', channel='toast')
 
 
