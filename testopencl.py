09f45fca1f5d5a4644d76e300e88cf56b42458c8
Vegnics <pe_linareso@hotmail.com>
2019-03-12 13:25:32 -0500

    Update testopencl.py

--- testopencl.py
+++ testopencl.py
@@ -3,13 +3,13 @@ import os
 def find_all(name, path):
     result = []
     for root, dirs, files in list(os.walk(path)):
-        if name in dirs:
+        if name in files:
             result.append(os.path.join(root, name))
             return result
 
-result=find_all('pip','/')
-a=os.popen('ls {}'.format(result[0]))
-result=a.read()
+result=find_all('install.py','/')
+#a=os.popen('ls {}'.format(result[0]))
+#result=a.read()
 CeleryPy.send_message(message='YEAH'+'\n'+str(result), message_type='success', channel='toast')
 
 
