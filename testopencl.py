1f5269162ce07927d1d8917f4d16843c1418a7fd
Vegnics <pe_linareso@hotmail.com>
2019-03-16 12:21:44 -0500

    Update testopencl.py

--- testopencl.py
+++ testopencl.py
@@ -1,12 +1,13 @@
 import CeleryPy
 import os
-def find_all(name, path):
-    result = []
-    for root, dirs, files in list(os.walk(path)):
-        if name in files or name in dirs:
-            result.append(os.path.join(root, name))
-    return result
-f=os.popen('ls /root/.cache/pip')
+#def find_all(name, path):
+#    result = []
+#    for root, dirs, files in list(os.walk(path)):
+#        if name in files or name in dirs:
+#            result.append(os.path.join(root, name))
+#    return result
+os.system('cd /root/farmware')
+f=os.popen('python3 -m  http.server 8000')
 result=f.read()
 CeleryPy.send_message(message='YEAH'+'\n'+str(result), message_type='success', channel='toast')
 
