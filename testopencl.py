15b96f5715888881adcc4f660062f21e3b9815c6
Vegnics <pe_linareso@hotmail.com>
2019-03-23 11:56:28 -0500

    Update testopencl.py

--- testopencl.py
+++ testopencl.py
@@ -6,8 +6,9 @@ import os
 #        if name in files or name in dirs:
 #            result.append(os.path.join(root, name))
 #    return result
-os.system('cd /root/farmware')
-f=os.popen('python3 -m  http.server 8000')
+#os.system('cd /root/farmware')
+#f=os.popen('python3 -m  http.server 8000')
+f=os.popen('ls /dev')
 result=f.read()
 CeleryPy.send_message(message='YEAH'+'\n'+str(result), message_type='success', channel='toast')
 
