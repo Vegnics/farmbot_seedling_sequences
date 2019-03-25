29a1d5aad14e58eda6e1b896ea7658b89f5d63ac
Vegnics <pe_linareso@hotmail.com>
2019-03-25 18:14:01 -0500

    Update find_holes.py

--- find_holes.py
+++ find_holes.py
@@ -84,7 +84,7 @@ def mergearrays(matrix1,matrix2):
     log(str(res_matrix.shape))
     return res_matrix
   
-#################################################################################################################################
+#######################################PRIMERA MATRIZ##########################################################################################
 number=1
 for num in range(number):
       CeleryPy.move_absolute((500,330,0),(0,0,0),150)
@@ -171,6 +171,74 @@ for num in range(number):
         log(str(matrix01.shape))
       matrix=mergearrays(matrix00,matrix01)
       np.save('/root/farmware/array',matrix)
+      #######________________SEGUNDA MATRIZ___________________________________-####################################
+      CeleryPy.move_absolute((845,300,0),(0,0,0),150)
+      CeleryPy.wait(5000)
+      file=Capture().capture()
+      img2 = cv2.imread(file,1)
+      image=invert(img2)
+      image_gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+      res = cv2.matchTemplate(image_gray,template,cv2.TM_CCOEFF_NORMED)
+      loc = np.where( res >= 0.75)
+      for pt in zip(*loc[::-1]):
+          cv2.circle(img2,(int(pt[0]+w/2),int(pt[1]+h/2)),15,(0,255,0),cv2.FILLED)
+      new_image=img2
+      cv2.imwrite('/tmp/images/1549138022.jpg',new_image)
+      mask=create_mask(new_image,np.array([HL,SL,VL]),np.array([HH,SH,VH]))###Creamos la máscara
+      image3=cv2.bitwise_and(new_image,new_image,mask=mask)##aplicamos la máscara
+      image3=cv2.medianBlur(image3,5)
+      cv2.imwrite('/tmp/images/1549138027.jpg',image3)
+
+      PD = PlantDetection(
+                  image='/tmp/images/1549138027.jpg',
+                  blur=2, morph=2, iterations=3, from_env_var=True, coordinates=True,
+                  array=[{"size": 5, "kernel": 'ellipse', "type": 'erode', "iters": 1}],
+                  HSV_min=[49,95,50],HSV_max=[110,255,255]
+                  )
+      PD.detect_plants() # detect coordinates and sizes of weeds and plants
+      if len(PD.plant_db.coordinate_locations) >= 1:
+        holes=[]
+        for coordinate_location in PD.plant_db.coordinate_locations:
+              if coordinate_location[2]>10.5:
+                holes.append([coordinate_location[0],coordinate_location[1]])  
+        rows,cols=array_shape(holes)
+        matrix10=np.zeros((rows,cols,2))
+        matrix10=fill_array(matrix10,holes) 
+        log(str(matrix10.shape))
+      #######___________________________________________________-####################################
+      CeleryPy.move_absolute((845,600,0),(0,0,0),150)
+      CeleryPy.wait(5000)
+      file=Capture().capture()
+      img2 = cv2.imread(file,1)
+      image=invert(img2)
+      image_gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+      res = cv2.matchTemplate(image_gray,template,cv2.TM_CCOEFF_NORMED)
+      loc = np.where( res >= 0.75)
+      for pt in zip(*loc[::-1]):
+          cv2.circle(img2,(int(pt[0]+w/2),int(pt[1]+h/2)),15,(0,255,0),cv2.FILLED)
+      new_image=img2
+      cv2.imwrite('/tmp/images/1549138022.jpg',new_image)
+      mask=create_mask(new_image,np.array([HL,SL,VL]),np.array([HH,SH,VH]))###Creamos la máscara
+      image3=cv2.bitwise_and(new_image,new_image,mask=mask)##aplicamos la máscara
+      image3=cv2.medianBlur(image3,5)
+      cv2.imwrite('/tmp/images/1549138027.jpg',image3)
+
+      PD = PlantDetection(
+                  image='/tmp/images/1549138027.jpg',
+                  blur=2, morph=2, iterations=3, from_env_var=True, coordinates=True,
+                  array=[{"size": 5, "kernel": 'ellipse', "type": 'erode', "iters": 1}],
+                  HSV_min=[49,95,50],HSV_max=[110,255,255]
+                  )
+      PD.detect_plants() # detect coordinates and sizes of weeds and plants
+      if len(PD.plant_db.coordinate_locations) >= 1:
+        holes=[]
+        for coordinate_location in PD.plant_db.coordinate_locations:
+              if coordinate_location[2]>10.5:
+                holes.append([coordinate_location[0],coordinate_location[1]])  
+        rows,cols=array_shape(holes)
+        matrix11=np.zeros((rows,cols,2))
+        matrix11=fill_array(matrix11,holes) 
+        log(str(matrix11.shape))
       if len(PD.plant_db.coordinate_locations) == 0:
         send_message(message='NO HOLES', message_type='error', channel='toast')
       CeleryPy.move_absolute((0,0,0),(0,0,0),200)
