250cf612ee9241e5b937a3192e50c4f115afcb30
Vegnics <pe_linareso@hotmail.com>
2019-03-26 09:08:04 -0500

    Update find_holes.py

--- find_holes.py
+++ find_holes.py
@@ -174,22 +174,15 @@ for num in range(number):
       matrix=mergearrays(matrix00,matrix01)
       np.save('/root/farmware/array',matrix)
       #######________________SEGUNDA MATRIZ___________________________________-####################################
-      CeleryPy.move_absolute((845,330,0),(0,0,0),150)
+      CeleryPy.move_absolute((845,290,0),(0,0,0),150)
       CeleryPy.wait(5000)
       file=Capture().capture()
       img2 = cv2.imread(file,1)
-      image=invert(img2)
-      image_gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-      w, h = template2.shape[::-1]
-      res = cv2.matchTemplate(image_gray,template2,cv2.TM_CCOEFF_NORMED)
-      loc = np.where( res >= 0.65)
-      for pt in zip(*loc[::-1]):
-          cv2.circle(img2,(int(pt[0]+w/2),int(pt[1]+h/2)),15,(0,255,0),cv2.FILLED)
-      
-      res = cv2.matchTemplate(image_gray,template3,cv2.TM_CCOEFF_NORMED)
-      loc = np.where( res >= 0.65)
-      for pt in zip(*loc[::-1]):
-          cv2.circle(img2,(int(pt[0]+w/2),int(pt[1]+h/2)),15,(0,255,0),cv2.FILLED)
+      image_gray=cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+      circles=cv2.HoughCircles(image_gray,cv2.HOUGH_GRADIENT,1,11,param1=65,param2=30,minRadius=15,maxRadius=34)
+      circles = np.uint16(np.around(circles))
+      for i in circles[0,:]:
+        cv2.circle(img2,(i[0],i[1]),15,(0,255,0),cv2.FILLED)
       new_image=img2
       cv2.imwrite('/tmp/images/1549138022.jpg',new_image)
       mask=create_mask(new_image,np.array([HL,SL,VL]),np.array([HH,SH,VH]))###Creamos la máscara
@@ -200,7 +193,7 @@ for num in range(number):
       PD = PlantDetection(
                   image='/tmp/images/1549138027.jpg',
                   blur=2, morph=2, iterations=3, from_env_var=True, coordinates=True,
-                  array=[{"size": 5, "kernel": 'ellipse', "type": 'erode', "iters": 6}],
+                  array=[{"size": 5, "kernel": 'ellipse', "type": 'erode', "iters": 1}],
                   HSV_min=[49,95,50],HSV_max=[110,255,255]
                   )
       PD.detect_plants() # detect coordinates and sizes of weeds and plants
