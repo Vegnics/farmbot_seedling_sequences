fabdc31accdcb9382e1207e406184e0171c5947d
Vegnics <pe_linareso@hotmail.com>
2019-03-25 19:37:06 -0500

    Update find_holes.py

--- find_holes.py
+++ find_holes.py
@@ -200,7 +200,7 @@ for num in range(number):
       PD = PlantDetection(
                   image='/tmp/images/1549138027.jpg',
                   blur=2, morph=2, iterations=3, from_env_var=True, coordinates=True,
-                  array=[{"size": 5, "kernel": 'ellipse', "type": 'erode', "iters": 1}],
+                  array=[{"size": 5, "kernel": 'ellipse', "type": 'erode', "iters": 2}],
                   HSV_min=[49,95,50],HSV_max=[110,255,255]
                   )
       PD.detect_plants() # detect coordinates and sizes of weeds and plants
@@ -240,7 +240,7 @@ for num in range(number):
       PD = PlantDetection(
                   image='/tmp/images/1549138027.jpg',
                   blur=2, morph=2, iterations=3, from_env_var=True, coordinates=True,
-                  array=[{"size": 5, "kernel": 'ellipse', "type": 'erode', "iters": 1}],
+                  array=[{"size": 5, "kernel": 'ellipse', "type": 'erode', "iters": 2}],
                   HSV_min=[49,95,50],HSV_max=[110,255,255]
                   )
       PD.detect_plants() # detect coordinates and sizes of weeds and plants
