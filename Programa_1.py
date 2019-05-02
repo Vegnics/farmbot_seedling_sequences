a03faffa7003c5e685137b2d214bbf89962cbc0c
Vegnics <pe_linareso@hotmail.com>
2019-05-02 13:28:17 -0500

    Update Programa_1.py

--- Programa_1.py
+++ Programa_1.py
@@ -69,7 +69,7 @@ VH=255
 
 kernel=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(4,4))
 mask = create_mask(new_image, np.array([45, 85, 50]), np.array([90, 255, 255]))
-mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=5)
+mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=3)
 mask = cv2.dilate(mask,kernel,iterations=2)
 mask = cv2.erode(mask,kernel,iterations=1)
 image3=cv2.bitwise_and(new_image,new_image,mask=mask)##aplicamos la máscara
@@ -81,7 +81,7 @@ PD = PlantDetection(
             blur=0, morph=2, iterations=1, from_env_var=True, coordinates=True,
             array=[{"size": 3, "kernel": 'ellipse', "type": 'dilate',  "iters": 3},
                    {"size": 3, "kernel": 'ellipse', "type": 'erode', "iters": 2}],
-            HSV_min=[45,75,40],HSV_max=[90,255,255]
+            HSV_min=[45,75,40],HSV_max=[95,255,255]
             )
 PD.detect_plants() # detect coordinates and sizes of weeds and plants
 
