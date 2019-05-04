524e5df3b5f797bcd0b21a05ea084186a51d5e7e
Vegnics <pe_linareso@hotmail.com>
2019-05-04 12:57:42 -0500

    Update cam_cal_script.py

--- cam_cal_script.py
+++ cam_cal_script.py
@@ -19,7 +19,7 @@ import time
 #y=x.get_image(95)
 device.set_pin_io_mode(1,4)
 weeder=(20,553,-402)
-CeleryPy.move_absolute((500,500,0),(0,0,0),150)
+CeleryPy.move_absolute((500,440,0),(0,0,0),150)
 #send_message(message=str(os.environ), message_type='success', channel='toast')
 file=Capture().capture()
 start=time.time()
@@ -28,12 +28,36 @@ def create_mask(image,lowergreen,uppergreen):##función para crear máscara a pa
   imghsv=cv2.cvtColor(image,cv2.COLOR_BGR2HSV_FULL)
   mask=cv2.inRange(imghsv,lowergreen,uppergreen)
   return mask
-def colorize(image):##función para cambiar el brillo y el contraste de imagen
- n_image = np.zeros(image.shape, image.dtype)
- alpha=2.4
- beta=-280
- n_image = np.clip(np.multiply(alpha,image)+beta, 0, 255)
- return n_image.astype(np.uint8) 
+def normalize(array):
+    b, g, r = cv2.split(array)
+    minimum=np.min(b)
+    maximum=np.max(b)
+    b_n=np.clip((b-minimum)*(255/maximum),0,255).astype(np.uint8)
+    minimum=np.min(g)
+    maximum=np.max(g)
+    g_n=np.clip((g-minimum)*(255/maximum),0,255).astype(np.uint8)
+    minimum=np.min(r)
+    maximum=np.max(r)
+    r_n=np.clip((r-minimum)*(255/maximum),0,255).astype(np.uint8)
+    array=cv2.merge([b_n, g_n, r_n])
+    return array
+def colorize(image,alpha,beta,theta):##función para cambiar el brillo y el contraste de imagen
+   #alpha = 1.32
+   #beta=60
+   #theta=1.8
+   n_image = alpha**(image/beta+theta)#+theta
+   n_image=normalize(n_image)
+   b,g,r=cv2.split(n_image)
+   kernel=(1/16)*np.ones([4,4],dtype=np.uint8)
+   b_n=cv2.filter2D(b,-1,kernel)
+   g_n=cv2.filter2D(g,-1,kernel)
+   r_n=cv2.filter2D(r,-1,kernel)
+   f=0.6
+   B=b-f*b_n
+   G=g-f*g_n
+   R=r-f*r_n
+   n_image=cv2.merge([B,G,R])+20
+   return n_image.astype(np.uint8)
 def invert(imagem):
  imagem = (255-imagem)
  return imagem
@@ -41,30 +65,23 @@ def invert(imagem):
 
 new_image=colorize(img2)##obtenemos imagen con brillo y contraste modificados
 cv2.imwrite('/tmp/images/1549138022.jpg',new_image)
-########SETEAMOS VALORES MÍNIMOS Y MÁXIMOS DE HSV##################
-HL=52
-SL=95
-VL=50
-HH=110
-SH=255
-VH=255
-###################################################################
-mask=create_mask(new_image,np.array([HL,SL,VL]),np.array([HH,SH,VH]))###Creamos la máscara
+kernel=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(4,4))
+mask = create_mask(new_image, np.array([45, 85, 50]), np.array([90, 255, 255]))
+mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=3)
+mask = cv2.dilate(mask,kernel,iterations=2)
 image3=cv2.bitwise_and(new_image,new_image,mask=mask)##aplicamos la máscara
 image3=cv2.medianBlur(image3,5)
 cv2.imwrite('/tmp/images/1549138027.jpg',image3)
 
 PD = PlantDetection(
             image='/tmp/images/1549138027.jpg',
-            blur=5, morph=2, iterations=5, from_env_var=True, coordinates=True,
+            blur=0, morph=2, iterations=1, from_env_var=True, coordinates=True,
             array=[{"size": 3, "kernel": 'ellipse', "type": 'dilate',  "iters": 3},
-                   {"size": 5, "kernel": 'ellipse', "type": 'erode', "iters": 3}],
-            HSV_min=[49,95,50],HSV_max=[110,255,255]
+                   {"size": 3, "kernel": 'ellipse', "type": 'erode', "iters": 3}],
+            HSV_min=[50,65,45],HSV_max=[90,255,255]
             )
-PD.detect_plants() # detect coordinates and sizes of weeds and plants
-end=time.time()
-milis=end-start
-log("TIEMPO DE PROCESAMIENTO: {}".format(milis))
+PD.detect_plants()
+
 if len(PD.plant_db.coordinate_locations) >= 1:
   #O=len(PD.plant_db.coordinate_locations)
   dir_path='/root/farmware'
