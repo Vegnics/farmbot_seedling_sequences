4d1759efa6561d32bb4243d29cf096ca08130592
Vegnics <pe_linareso@hotmail.com>
2019-05-27 09:49:41 -0500

    Update Programa_2.py

--- Programa_2.py
+++ Programa_2.py
@@ -46,8 +46,8 @@ xsig=xsig-4
 ysig=ysig+8
 x=x-7
 y=y+7
-CeleryPy.move_absolute((x-22,y,-205),(0,0,0),100)
-CeleryPy.move_absolute((x-22,y,-275),(0,0,0),100)
+CeleryPy.move_absolute((x-22,y-10,-205),(0,0,0),100)
+CeleryPy.move_absolute((x-22,y-10,-275),(0,0,0),100)
 CeleryPy.move_absolute((x,y,-281),(0,0,0),100)
 CeleryPy.wait(500)
 CeleryPy.write_pin(number=4, value=0, mode=0)
