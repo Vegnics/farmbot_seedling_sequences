7414b3f66c428146e6850910a7b67e73d9a1ad26
Vegnics <pe_linareso@hotmail.com>
2019-11-05 12:19:50 -0500

    Create Traslado_bandeja_bandeja.py

---
+++ Traslado_bandeja_bandeja.py
@@ -0,0 +1,87 @@
+import os
+import sys
+from CeleryPy import log
+from CeleryPy import send_message
+from DB import DB
+from Image import Image
+import json
+from Parameters import Parameters
+import requests
+import cv2
+from Capture import Capture
+import numpy as np
+from PlantDetection import PlantDetection
+from farmware_tools import device
+from farmware_tools import get_config_value
+import CeleryPy
+import time
+
+farmware_name ='Seedling transfering'
+origin_tray = get_config_value(farmware_name, config_name='Origin_tray')
+destiny_tray = get_config_value(farmware_name, config_name='Destiny_tray')
+weeder = (20, 553, -402)
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+if origin_tray>0 and origin_tray<=4 and destiny_tray>0 and destiny_tray<=4 and origin_tray!=destiny_tray:
+    matrix = np.load(dir_path + '/' + 'array'+str(origin_tray)+'.npy')
+    matrix2 = np.load(dir_path + '/' + 'array'+str(destiny_tray)+'.npy')
+    CeleryPy.move_absolute(weeder, (0, 0, 0), 150)
+    CeleryPy.move_absolute(weeder, (100, 0, 0), 150)
+    CeleryPy.move_absolute(weeder, (100, 0, 100), 150)
+    CeleryPy.move_absolute(weeder, (100, 0, 200), 150)
+    CeleryPy.write_pin(number=4, value=1, mode=0)
+    CeleryPy.wait(100)
+    CeleryPy.write_pin(number=4, value=0, mode=0)
+    CeleryPy.wait(200)
+    CeleryPy.write_pin(number=4, value=1, mode=0)
+
+
+    for _row in range(5):
+        for _column in range(11):
+            xmat = _row
+            ymat = _column
+            xmatsig = _row
+            ymatsig = _column
+            x, y = matrix[ymat, xmat]
+            xsig, ysig = matrix2[ymatsig, xmatsig]
+            xsig = xsig - 4
+            ysig = ysig + 4
+            x = x - 7
+            y = y + 7
+            CeleryPy.move_absolute((x - 22, y - 10, -205), (0, 0, 0), 100)
+            CeleryPy.move_absolute((x - 22, y - 10, -265), (0, 0, 0), 100)
+            CeleryPy.move_absolute((x, y, -265), (0, 0, 0), 100)
+            CeleryPy.move_absolute((x, y, -290), (0, 0, 0), 100)
+            CeleryPy.wait(500)
+            CeleryPy.write_pin(number=4, value=0, mode=0)
+            CeleryPy.wait(2000)
+            CeleryPy.move_absolute((x, y, -115), (0, 0, 0), 100)
+            CeleryPy.wait(500)
+            CeleryPy.move_absolute((xsig, ysig, -115), (0, 0, 0), 100)
+            CeleryPy.move_absolute((xsig, ysig, -278), (0, 0, 0), 100)
+            CeleryPy.write_pin(number=4, value=1, mode=0)
+            CeleryPy.wait(400)
+            CeleryPy.write_pin(number=4, value=0, mode=0)
+            CeleryPy.wait(400)
+            CeleryPy.write_pin(number=4, value=1, mode=0)
+            CeleryPy.wait(400)
+            CeleryPy.write_pin(number=4, value=0, mode=0)
+            CeleryPy.wait(400)
+            CeleryPy.write_pin(number=4, value=1, mode=0)
+            CeleryPy.move_absolute((xsig, ysig, -205), (0, 0, 0), 100)
+            CeleryPy.write_pin(number=4, value=0, mode=0)
+            CeleryPy.wait(200)
+            CeleryPy.write_pin(number=4, value=1, mode=0)
+            CeleryPy.wait(200)
+            CeleryPy.write_pin(number=4, value=0, mode=0)
+            CeleryPy.wait(200)
+            CeleryPy.write_pin(number=4, value=1, mode=0)
+            CeleryPy.wait(500)
+    CeleryPy.move_absolute(weeder, (120, 0, 200), 150)
+    CeleryPy.move_absolute(weeder, (120, 0, 0), 150)
+    CeleryPy.move_absolute(weeder, (0, 0, 0), 150)
+    CeleryPy.move_absolute(weeder, (0, 0, 200), 150)
+    send_message(message='Succesful Sequence', message_type='success', channel='toast')
+    CeleryPy.move_absolute((0, 0, 0), (0, 0, 0), 250)
+else:
+    send_message(message='Tray number error', message_type='error', channel='toast')
