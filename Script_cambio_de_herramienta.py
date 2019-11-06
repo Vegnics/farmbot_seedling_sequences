b83edb9878f4d247995e0e1ed778f2221d18738f
Vegnics <pe_linareso@hotmail.com>
2019-11-06 15:52:39 -0500

    Update Script_cambio_de_herramienta.py

--- Script_cambio_de_herramienta.py
+++ Script_cambio_de_herramienta.py
@@ -1,1 +1,13 @@
 import os
+import sys
+from CeleryPy import log
+from CeleryPy import send_message
+import numpy as np
+from farmware_tools import device
+import CeleryPy
+
+
+device.set_pin_io_mode(1,4)
+weeder=(20,553,-402)
+herramienta_2=()#modificar posición de herramienta 2
+CeleryPy.move_absolute((500,440,0),(0,0,0),150)
