97b7748763edef527b07d695660e0480eb78143a
Vegnics <pe_linareso@hotmail.com>
2019-12-03 18:59:24 -0500

    Update scriptprincipal.py

--- scriptprincipal.py
+++ scriptprincipal.py
@@ -18,7 +18,7 @@ import time
 #x=DB()
 #y=x.get_image(95)
 device.set_pin_io_mode(1,4)
-weeder=(20,553+4,-402)
+weeder=(20,553,-402)
 CeleryPy.move_absolute((500,440,0),(0,0,0),150)
 #send_message(message=str(os.environ), message_type='success', channel='toast')
 file=Capture().capture()
