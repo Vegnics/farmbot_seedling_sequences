c51b6e788871261b3e61a7b716edc162ae86972d
Vegnics <pe_linareso@hotmail.com>
2021-01-14 12:56:21 -0500

    Update takephoto.py

--- takephoto.py
+++ takephoto.py
@@ -21,12 +21,12 @@ def usb_camera_photo():
     discard_frames = 10  # number of frames to discard for auto-adjust
     max_attempts = 5     # number of failed discard frames before quit
     cam = cv2.VideoCapture(0)
-    cam.set(cv2.CAP_PROP_FRAME_WIDTH,640)#640
-    cam.set(cv2.CAP_PROP_FRAME_HEIGHT,480)#480
-    cam.set(cv2.CAP_PROP_BRIGHTNESS,0.5)#0.5
-    cam.set(cv2.CAP_PROP_CONTRAST,0.73333)#0.733333
-    cam.set(cv2.CAP_PROP_SATURATION,0.3543)#0.3543
-    cam.set(cv2.CAP_PROP_HUE,0.5)#0.5
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH,width)#640
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)#480
+    cam.set(cv2.CAP_PROP_BRIGHTNESS,bright*0.01)#0.5
+    cam.set(cv2.CAP_PROP_CONTRAST,contrast*0.01)#0.733333
+    cam.set(cv2.CAP_PROP_SATURATION,saturation*0.01)#0.3543
+    cam.set(cv2.CAP_PROP_HUE,hue*0.01)#0.5
     device.log(message='setting ok', message_type='success')
     #send_message(message='{}'.format(cam.get(cv2.CAP_PROP_FRAME_WIDTH)), message_type='success', channel='toast')
     #send_message(message='{}'.format(cam.get(cv2.CAP_PROP_FRAME_HEIGHT)), message_type='success', channel='toast')
@@ -37,7 +37,7 @@ def usb_camera_photo():
     failed_attempts = 0
     max_attempts = 5
     
-    for a in range(20):
+    for a in range(10):
         ret,image = cam.read()
         if not cam.grab():
             #verbose_log('Could not get frame.')
